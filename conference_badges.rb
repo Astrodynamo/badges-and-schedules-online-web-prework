a76581d985faa00f19404974bfd148025131a0cc
Amanda King <amapickell@gmail.com>
2019-05-23 19:17:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker (name)
+  phrase = "Hello, my name is #{name}."
+  return phrase
+end
+
+def batch_badge_creator (array)
+  badges = []
+  
+end
+
+def assign_rooms
+  
+end
+
+def printer
+  
+end
